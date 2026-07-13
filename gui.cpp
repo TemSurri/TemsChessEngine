eb1a6a4936f73cc68b677f092f1d2f3d0e965c9b
Temsurri <tarantem@icloud.com>
2026-07-13 13:06:33 -0400

    drew a tiranlge

--- gui.cpp
+++ gui.cpp
@@ -1,6 +1,26 @@
 #include "gui.h"
 #include <iostream>
 
+const char* vertexShaderSource = R"(#version 330 core
+
+layout (location = 0) in vec3 aPos;
+
+void main()
+{
+    gl_Position = vec4(aPos, 1.0);
+}
+)";
+
+const char* fragmentShaderSource = R"(#version 330 core
+
+out vec4 FragColor;
+
+void main()
+{
+    FragColor = vec4(0.2, 0.8, 0.3, 1.0);
+}
+)";
+
 GLFWwindow* GuiManager::guiWindowSetUp() {
 
     // init glfw
@@ -62,7 +82,73 @@ int GuiManager::guiMainLoop()
     glfwSwapBuffers(window);
     //--------------------------------------------
 
+    //draw mat
+    GLfloat vertices[] = {
+
+        -0.5f, -0.1f, 0.0f,
+
+        -0.3f, -0.8f, 0.0f,
+
+        -0.1f, -0.2f, 0.0f
+
+    };
+
+    //setting up shaders
+    GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
+
+    GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+    glCompileShader(fragmentShader);
+
+    GLuint shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+
+    glLinkProgram(shaderProgram);
+
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
+    //buffers = big batches of data
+    //creating buffers
+
+    GLuint VAO, VBO;
+
+    //VAO is a way to show how to use the data stored in VBO
+    glGenVertexArrays(1, &VAO);
+
+    //VBO is a way to simpley story the data in a state openGl can use
+    glGenBuffers(1, &VBO);
+
+    glBindVertexArray(VAO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+   
+    glEnableVertexAttribArray(0);
+
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
+
+
     while (!glfwWindowShouldClose(window)) {
+        
+        glClearColor(0.42f, 0.13f, 0.11f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glUseProgram(shaderProgram);
+        glBindVertexArray(VAO);
+
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+
+
+
+        glfwSwapBuffers(window);
+
+
 
         glfwPollEvents();
 
@@ -70,7 +156,12 @@ int GuiManager::guiMainLoop()
 
     }
 
-    //clean up
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+    glDeleteProgram(shaderProgram);
+
+
+    //clean up window
     guiWindowCleanUp(window);
     return 0;
 }
